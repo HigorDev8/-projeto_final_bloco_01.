81b13eab463b0fb00c389b9e20e890e7fe9137de
HigorDev8 <higordev8@gmail.com>
2024-03-13 11:29:28 -0300

    projeto_final_bloco_01

--- src/menu/Produto.java
+++ src/menu/Produto.java
@@ -1,5 +1,22 @@
 package menu;
 
-public class Produto {
+public abstract class Produto {
+	
+	private String nome;
+	private double preco;
+	
+	public Produto(String nome, double preco) {
+		
+		this.nome = nome;
+		this.preco = preco;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	
 }
